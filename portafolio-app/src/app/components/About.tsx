47cae0ba8c502031e9376437b8109e1e8d8b3d74
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-06 14:13:15 -0600

    Botton About
    Se creo un boton en el apartado about con boostrap

--- portafolio-app/src/app/components/About.tsx
+++ portafolio-app/src/app/components/About.tsx
@@ -1,19 +1,23 @@
 "use client"
 import React from 'react'
-import Profile from "../components/Logo Andrey.png"
+import Profile from "../components/Perfil Andrey.jpeg"
 import Image from "next/image";
+import { Button } from "react-bootstrap"
 
 const About = () => {
   return (
     <div className="containers">
       <figure className="Image">
-        <Image src={Profile} alt="" width={400} height={250} />
+        <Image src={Profile} alt="" width={500} height={300} />
       </figure>
       <h2>I’m Andrey, and I like to
         <br /> work on your next project</h2>
       <p>I like working with others to create design solutions
         <br />I have done some projects
         <br />most college jobs</p>
+      <div className="Button">
+        <Button variant="dark" size="lg">Free consultion</Button>
+      </div>
     </div>
   )
 }
