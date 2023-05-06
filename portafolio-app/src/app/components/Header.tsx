58a0fd8367c9ed528793ec50c71b967d880ca59b
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-06 11:27:36 -0600

    Header
    se creo el Header con sus estilos css

--- portafolio-app/src/app/components/Header.tsx
+++ portafolio-app/src/app/components/Header.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
 
-        <h1>Hello Iam Andrey</h1>
+    <div className="container">
+        <h1>Hello this is Andrey Portfolio</h1>
+        <p>If you are interested in my portfolio, do not hesitate to follow me for more information.</p>
+        <p>portfolio for your design needs</p>
     </div>
   )
 }
