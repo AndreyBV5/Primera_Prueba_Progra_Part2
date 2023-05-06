dd3f0b8356e0c862cd8eac011a51ef8fe7db2f41
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-06 10:17:01 -0600

    Navbar
    se creo un navbar con boostrap

---
+++ portafolio-app/src/app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client"
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import {Nav, Navbar} from "react-bootstrap"
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default ColorSchemesExample;
--- portafolio-app/src/app/page.tsx
+++ portafolio-app/src/app/page.tsx
@@ -1,24 +1,23 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+"use client"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
+import ColorSchemesExample from './components/Navbar'
+
 
 const page = () => {
   return (
     <div>
+      <ColorSchemesExample/>
       <Header/>
       <Hero/>
       <About/>
       <Projects/>
       <Footer/>
-
-
-
     </div>
   )
 }
