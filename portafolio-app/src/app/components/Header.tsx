09bcead122f3ca821b49444a4fee7b10e0633cff
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-06 20:52:56 -0600

    Logo github y Update
    se creo un logo que se puso al lado del titulo del header y también se actualizaron algunos diseños css en general

--- portafolio-app/src/app/components/Header.tsx
+++ portafolio-app/src/app/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Image from "next/image";
+import Logo from '../components/Logo_github.png'
 
 const Header = () => {
   return (
 
     <div className="container">
+      <div className='HeaderImage'>
+      <Image src={Logo} alt="" width={50} height={50} />
+      </div>
         <h1>Andrey Portfolio</h1>
         <p>If you are interested in my portfolio, do not hesitate to follow me for more information.</p>
         <div className="secondP"><p >portfolio for your design needs</p></div>
